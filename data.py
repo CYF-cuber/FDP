60747d2623b740a5bb95cd56a751bea7802a7165
CYF-cuber <85044231+CYF-cuber@users.noreply.github.com>
2024-11-26 16:53:45 +0800

    Update data.py

--- data.py
+++ data.py
@@ -19,6 +19,10 @@ import math
 
 import sys
 
+image_rows, image_columns, image_depth = 64, 64, 8
+VIDEO_LENGTH = 8
+sequence_loss = nn.MSELoss(reduction="mean")#SeqLoss(test_mode=True)
+
 def get_dynamic_image(frames, normalized=True):
     """ Takes a list of frames and returns either a raw or normalized dynamic image."""
     num_channels = frames[0].shape[2]                                                        # num_channels = 3
@@ -61,7 +65,7 @@ def _compute_dynamic_image(frames):
         a3 = a1 * a2                                                 # (1,3,h,w,1) * (3,1,1,1) = (1,3,h,w,1)
         y = np.sum(a3[0], axis=0)                                    # y.shape = (h, w, 1)
     return y 
-    
+
 def align_face(img, img_land, box_enlarge, img_size):
 
     leftEye0 = (img_land[2 * 37] + img_land[2 * 38] + img_land[2 * 39] + img_land[2 * 40] + img_land[2 * 41] +
@@ -75,8 +79,7 @@ def align_face(img, img_land, box_enlarge, img_size):
     deltaX = float(rightEye0 - leftEye0)
     deltaY = float(rightEye1 - leftEye1)
     l = math.sqrt(deltaX * deltaX + deltaY * deltaY)
-    #print("pupil distance:",l)
-    #print(deltaX,deltaY)
+
     sinVal = deltaY / l
     cosVal = deltaX / l
     mat1 = np.mat([[cosVal, sinVal, 0], [-sinVal, cosVal, 0], [0, 0, 1]])
@@ -97,8 +100,7 @@ def align_face(img, img_land, box_enlarge, img_size):
     scale = (img_size - 1) / 2.0 / halfSize
     mat3 = np.mat([[scale, 0, scale * (halfSize - cx)], [0, scale, scale * (halfSize - cy)], [0, 0, 1]])
     mat = mat3 * mat1
-    #print((mat[0:2, :]))
-    #print(mat1)
+
     aligned_img = cv2.warpAffine(img, mat[0:2, :], (img_size, img_size), cv2.INTER_LINEAR, borderValue=(128, 128, 128))
 
     land_3d = np.ones((int(len(img_land)/2), 3))
@@ -109,10 +111,11 @@ def align_face(img, img_land, box_enlarge, img_size):
 
     return aligned_img, new_land
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('../shape_predictor_68_face_landmarks.dat')
 
-def img_pre_dlib(detector,predictor,img_path,box_enlarge=2.0,img_size=64):
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+
+def img_pre_dlib(detector,predictor,img_path,box_enlarge=2.5,img_size=64):
     img = cv2.imread(img_path)#[:,80:560]
 
     img_dlib = dlib.load_rgb_image(img_path)
@@ -122,27 +125,42 @@ def img_pre_dlib(detector,predictor,img_path,box_enlarge=2.0,img_size=64):
     ldm=ldm.reshape(136,1)
 
     aligned_img, new_land = align_face(img, ldm, box_enlarge, img_size)
-    return aligned_img
+    new_land = new_land.reshape(1,136)
+    return aligned_img , new_land
+
+def image_pro(img):
+    normalize = transforms.Normalize(mean=[0.5],std=[0.5])
+
+    transform_1 = transforms.Compose([
+        transforms.Grayscale(1),
+        transforms.ToTensor()
+    ])
+    transform_2 = transforms.Compose([
+        transforms.Grayscale(1),
+        transforms.ToTensor(),
+        normalize
+    ])
+    gray_img = transform_1(img)
+    norm_gray_img = transform_2(img)
+    #print(norm_img.size())
+    return gray_img, norm_gray_img
 
 class videoDataset(Dataset):
-    def __init__(self, video_path):
+    def __init__(self, video_path, load_data=False, mode_train =True):
         self.video_list = []
+        self.dyimg_list = []
         self.video_labels = []
+        self.mode_train = mode_train
 
         for video in range(len(video_path)):
-            self.load_class_video(video_path[video], class_num = video)
-        
+            self.load_class_video(video_path[video], load=load_data , mode_train =self.mode_train , class_num = video)
         self.video_list = np.asarray(self.video_list)
-        print(self.video_list.shape)
 
+        self.dyimg_list = np.asarray(self.dyimg_list)
         self.training_samples = len(self.video_list)
 
-        #self.video_list = np.expand_dims(self.video_list, axis=1).astype('float32')
-        self.video_list = self.video_list.astype(np.float32)
-        print(self.video_list)
-        print(self.video_labels)
-        
-        print(self.video_list.shape)
+        self.video_list = np.expand_dims(self.video_list, axis=1).astype('float32')
+
         self.video_list = self.video_list - np.mean(self.video_list)
         self.video_list = self.video_list / np.max(self.video_list)
 
@@ -150,50 +168,62 @@ class videoDataset(Dataset):
         return len(self.video_list)
 
     def __getitem__(self, item):
-        return self.video_list[item], self.video_labels[item]
+        return self.video_list[item], self.dyimg_list[item], self.video_labels[item]
+
 
     def to_categorical(self, y, num_classes):
         """ 1-hot encodes a tensor """
         return np.eye(num_classes, dtype='uint8')[y]
 
-    def video_length_judge(self, framelist):
+    def video_length_judge(self, video_path, framelist):
         video_len = len(framelist)
-        if video_len <8 :
+        if video_len <VIDEO_LENGTH :
+            print(video_path + " is < 8 frames!")
             return False
         else:
             return True
 
-    def load_video(self, video_path, framelist):
+    def load_video(self, video_path, framelist, load_frame_data=False, video_name='',mode_train=True):
 
         video_len = len(framelist)
 
-        sample_time = video_len // 8
+        sample_time = video_len // VIDEO_LENGTH
         frames = []
+        frames_dyimg = []
 
-        for i in range(8):
+        for i in range(VIDEO_LENGTH):
             img_path = video_path + '/' + framelist[i * sample_time]
-            align_img = img_pre_dlib(detector,predictor,img_path)
-            print(align_img.shape)
-            #cv2.imwrite("/mnt/data/experiments/micro-expression-recognition-master/Frequency_MER/img/test_ali.jpg",align_img)
+            align_img,ldm = img_pre_dlib(detector,predictor,img_path)
+            #print(align_img.shape)
+            image =  Image.fromarray(cv2.cvtColor(align_img,cv2.COLOR_BGR2RGB))
+            #print(type(image))
+            gray_img, norm_gray_img = image_pro(image)
+            frames_dyimg.append(gray_img.numpy())
+            frames.append(norm_gray_img.numpy())
 
-            frames.append(align_img)
-            
-        #dy_img = get_dynamic_image(frames_dyimg)
-        
+        frames_dyimg = np.asarray(frames_dyimg).reshape(8,64,64,1)
+
+        dy_img = get_dynamic_image(frames_dyimg)
+
         frames = np.asarray(frames)
-        print(frames.shape)
-        
-        return frames
 
-    def load_class_video(self, video_path,class_num = None):
+        return frames, dy_img
 
+    def load_class_video(self, video_path, load=False,mode_train = True,class_num = None):
+        #directorylisting = os.listdir(video_path)
         directorylisting = video_path
         for video in directorylisting:
 
             videopath = video_list[class_num] + video
-  
+            #print(videopath)
+            '''
+            if not os.path.exists(videopath):
+                videopath = video_list[class_num+3] +video
+            '''
             print(videopath)
             framelist = os.listdir(videopath)
+
+            #framelist.sort(key=lambda x: int(x.split('img')[1].split('.jpg')[0]))
             
             if "EP" in video:
                 framelist.sort(key=lambda x: int(x.split('img')[1].split('.jpg')[0]))
@@ -202,79 +232,67 @@ class videoDataset(Dataset):
             else:
                 framelist.sort(key=lambda x: int(x.split('.')[0]))
 
-            if self.video_length_judge( framelist) is False:
+            if self.video_length_judge(videopath, framelist) is False:
                 continue
 
-            videoarray= self.load_video(videopath, framelist)
-            
-            videoarray = np.rollaxis(videoarray,3,0)
-            print(videoarray.shape)
+            if mode_train:
+                videoarray, dy_img = self.load_video(videopath, framelist, load_frame_data=False, video_name=video,mode_train=True)
+            else:
+                videoarray, dy_img = self.load_video(videopath, framelist, load_frame_data=False, video_name=video,mode_train=False)
 
             if len(videoarray) <= 0:
                 print("video invalid!")
                 continue
             self.video_list.append(videoarray)
+            self.dyimg_list.append(dy_img)
             # 添加标签
-            self.video_labels.append(class_num % 3)
+            self.video_labels.append(class_num % 5)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default="SAMM")
     parser.add_argument('--net_test',action='store_true')
     args = parser.parse_args()
+
     
     if args.dataset == "CASME2":
-        surprisepath_c = '../CASME2_data_3/surprise/'
-        positivepath_c = '../CASME2_data_3/positive/'
-        negativepath_c = '../CASME2_data_3/negative/'
-        video_list = [surprisepath_c, positivepath_c, negativepath_c]
+        surprise_path = './CASME2_data_5/surprise/'
+        happiness_path = './CASME2_data_5/happiness/'
+        disgust_path = './CASME2_data_5/disgust/'
+        repression_path = './CASME2_data_5/repression/'
+        others_path = './CASME2_data_5/others/'
+        video_list = [surprise_path , happiness_path, disgust_path , repression_path , others_path]
         LOSO = ['17', '26', '16', '09', '05', '24', '02', '13', '04', '23', '11', '12', '08', '14', '03', '19', '01', '10', '20', '21', '22', '15', '06', '25', '07']
-        #[7, 8, 10, 12, 13, 17, 18, 19, 20, 24]
-        LOSO = ['17', '26', '16', '09', '05', '24', '02', '23', '12',  '03', '19', '01', '15', '06', '25']
-
-
     if args.dataset == "SAMM":
-        surprisepath_s = '../SAMM_data_3/surprise/'
-        positivepath_s = '../SAMM_data_3/positive/'
-        negativepath_s = '../SAMM_data_3/negative/'
-        video_list = [surprisepath_s, positivepath_s, negativepath_s]
-        LOSO =['006','007','009','010','011','012','013','014','015','016','017','018','019','020','021','022','023','024','026','028','030','031','032','033','034','035','036','037']
-        #[3, 6, 12,  14,  16, 17, 18, 19,  20,  21, 22,  24,  26,  27]
-        LOSO =['006','007','009','011','012','014','015','016','017','018','020','022','033','035']
+        surprise_path = './SAMM_data_5/surprise/'
+        happiness_path = './SAMM_data_5/happiness/'
+        anger_path = './SAMM_data_5/anger/'
+        contempt_path = './SAMM_data_5/contempt/'
+        others_path = './SAMM_data_5/others/'
+        video_list = [surprise_path , happiness_path, anger_path , contempt_path , others_path]
+        #LOSO =['006','007']
+        LOSO =['006','007','009','010','011','012','013','014','015','016','017','018','019','020','021','022','023','025','026','028','030','031','032','033','034','035','036','037']
 
 
-    '''##################################################################################
-    If you want to create 5-cls dataset, use Line 205-223 instead of 182-199 and change Line 174 to "            self.video_labels.append(class_num % 5)" and Line 233 to "        test_list = [[],[],[],[],[]]"
-    '''
-    # if args.dataset == "CASME2":
-    #     surprise_path = '../CASME2_data_5/surprise/'
-    #     happiness_path = '../CASME2_data_5/happiness/'
-    #     disgust_path = '../CASME2_data_5/disgust/'
-    #     repression_path = '../CASME2_data_5/repression/'
-    #     others_path = '../CASME2_data_5/others/'
-    #     video_list = [surprise_path , happiness_path, disgust_path , repression_path , others_path]
-    #     LOSO = ['01', '17', '26', '16', '09', '05', '24', '02', '13', '04', '23', '11', '12', '08', '14', '03', '19', '10', '20', '21', '22', '15', '06', '25', '07']
-    #     #LOSO = ['25', '07']
-    # if args.dataset == "SAMM":
-    #     surprise_path = '../SAMM_data_5/surprise/'
-    #     happiness_path = '../SAMM_data_5/happiness/'
-    #     anger_path = '../SAMM_data_5/anger/'
-    #     contempt_path = '../SAMM_data_5/contempt/'
-    #     others_path = '../SAMM_data_5/others/'
-    #     video_list = [surprise_path , happiness_path, anger_path , contempt_path , others_path]
-    #     #LOSO =['006','007','009','010','011','012','013','014','015','016','017','018','019','020','021','022','023','024','025','026','028','030','031','032','033','034','035','036','037']
-    #     # del '024'
-    #     LOSO =['006','007','009','010','011','012','013','014','015','016','017','018','019','020','021','022','023','025','026','028','030','031','032','033','034','035','036','037']
 
+    test_log_file = open('log/dataset_test_log.txt', 'w')
+    train_log_file = open('log/dataset_train_log.txt', 'w')
+
+    train_log_file.writelines('----------args----------\n')
+    test_log_file.writelines('----------args----------\n')
+    for k in list(vars(args).keys()):
+        print('%s: %s' % (k, vars(args)[k]))
+        train_log_file.writelines('%s: %s\n' % (k, vars(args)[k]))
+        test_log_file.writelines('%s: %s\n' % (k, vars(args)[k]))
+    train_log_file.writelines('----------args----------\n')
+    test_log_file.writelines('----------args----------\n')
 
     videos = [os.listdir(i) for i in video_list]
-    
-    one_cls_sub = []
-    
-    for sub in range(len(LOSO)):
 
+    for sub in range(len(LOSO)):
+        #model = nextvit_MER_v3()
         subject = LOSO[sub]
-        test_list = [[],[],[]]
+        test_list = [[],[],[],[],[]]
         for cla in range(len(videos)):
             class_video = videos[cla]
             for v in class_video:
@@ -282,14 +300,27 @@ if __name__ == "__main__":
                     test_list[cla].append(v)
 
         print(test_list)
-        
-        blank_count =0  
-        for c in test_list:
-            if len(c) == 0:
-                blank_count = blank_count+1
-            if blank_count == 2:
-                one_cls_sub.append(sub)
+        train_list = [[x for x in videos[y] if x not in test_list[y]] for y in [0,1,2,3,4]]
+        print(train_list)
         if args.net_test:
-            test_list = [c[0:1] for c in test_list]     
-        test_dataset =  videoDataset(test_list)
-        torch.save(test_dataset, './mer_dataset/FDP_'+args.dataset+'_3cls_sub'+subject+'.pth')
+            test_list = [c[0:1] for c in test_list]
+            #print(test_list)
+            train_list = [d[0:1] for d in train_list]
+        test_dataset =  videoDataset(test_list, load_data=True,mode_train=False)
+        train_dataset = videoDataset(train_list, load_data=True,mode_train=True)
+
+        train_dataset_size = train_dataset.__len__()
+        test_dataset_size = test_dataset.__len__()
+        train_log_file.writelines('train_dataset_size:' + str(train_dataset_size))
+        train_log_file.writelines('test_dataset_size:'+ str(test_dataset_size))
+        print('Generating dataset...')
+        print('train_dataset.size:{}'.format(train_dataset_size)+'\n')
+        print('test_dataset.size:{}'.format(test_dataset_size)+'\n')
+
+
+        print('Saving dataset...')
+        torch.save(train_dataset, 'dataset/FDP_'+args.dataset+'_train_'+subject+'subject_5cls.pth')
+        torch.save(test_dataset, 'dataset/FDP_'+args.dataset+'_test_'+subject+'subject_5cls.pth')
+        
+        train_log_file.writelines('LOSO ' +subject+'\n')# %s\n' % (str(i+1)))
+        test_log_file.writelines('LOSO '+subject+'\n')# %s\n' % (str(i+1)))
